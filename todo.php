1c8ee85671df03c218998e0fa1f125e42b74be8c
Shivadhar Pingili <sp2344@afsconnect1.njit.edu>
2017-11-20 02:19:52 -0500

    Active.

---
+++ todo.php
@@ -0,0 +1,18 @@
+<?php
+class todo extends model 
+ {
+  protected static $modelName = 'todo';
+  public $id;
+  public $owneremail;
+  public $ownerid;
+  public $createddate;
+  public $duedate;
+  public $message;
+  public $isdone;
+  public static function tableName()
+   {
+    $tableName='todos';
+    return $tableName;
+   }
+  }
+?>
